8c1757bbd6e9c7ea16d98e017526d8c0fd2132cf
Ashraf-2 <hasanashraful03@gmail.com>
2023-10-16 00:46:31 +0600

    added delete and put for update in bckend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,13 +37,47 @@ async function run() {
 
     })
 
+    app.get('/coffee/:id',async(req,res)=> {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await coffeeCollection.findOne(query);
+        res.send(result);
+    })
 
     app.post('/coffee', async(req,res)=> {
         const newCoffee = req.body;
         console.log(newCoffee);
-
         const result = await coffeeCollection.insertOne(newCoffee);
         res.send(result);
+    })
+
+
+    app.put('/coffee/:id',async(req,res)=> {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true};
+      const updatedCOffee = req.body;
+
+      const coffee1 = {
+        $set: {
+          name:updatedCOffee.name,
+          quantity:updatedCOffee.quantity,
+          supplier:updatedCOffee.supplier,
+          taste:updatedCOffee.taste,
+          details:updatedCOffee.details, 
+          category:updatedCOffee.category, 
+          photo_url:updatedCOffee.photo_url
+        }
+      }
+      const result  = await coffeeCollection.updateOne(filter,coffee1,options)
+      res.send(result);
+    })
+
+    app.delete('/coffee/:id',async(req,res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
     })
 
 
